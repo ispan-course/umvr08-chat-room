13edef0c2f52507b736921865502d6fd34962cc0
Arthur Pai <baidragoon@gmail.com>
2021-11-08 16:16:43 +0800

    feat: create connection

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -1,9 +1,45 @@
-﻿namespace ChatClient
+﻿using System;
+using System.Net.Sockets;
+
+namespace ChatClient
 {
   internal class Program
   {
     public static void Main(string[] args)
     {
+      const string hostIP = "127.0.0.1";
+      const int port = 4099;
+
+      Console.WriteLine("====================================");
+      var client = new TcpClient();
+
+      try
+      {
+        Console.WriteLine("Connecting to chat server {0}:{1}", hostIP, port);
+        client.Connect(hostIP, port);
+
+        if (!client.Connected)
+        {
+          Console.WriteLine("Can't Connect to chat server");
+          return;
+        }
+
+        Console.WriteLine("Connected to chat server");
+        }
+      }
+      catch (ArgumentNullException e)
+      {
+        Console.WriteLine("ArgumentNullException: {0}", e);
+      }
+      catch (SocketException e)
+      {
+        Console.WriteLine("SocketException: {0}", e);
+      }
+      finally
+      {
+        client.Close();
+        Console.WriteLine("Disconnected");
+      }
     }
   }
 }
--- ChatServer/Program.cs
+++ ChatServer/Program.cs
@@ -1,9 +1,42 @@
-﻿namespace ChatServer
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ChatServer
 {
   internal class Program
   {
     public static void Main(string[] args)
     {
+      const int port = 4099;
+
+      Console.WriteLine("====================================");
+      var listener = new TcpListener(IPAddress.Any, port);
+
+      try
+      {
+        Console.WriteLine("Server start at port {0}", port);
+        listener.Start();
+
+        Console.WriteLine("Waiting for a connection... ");
+        var client = listener.AcceptTcpClient();
+
+        var address = client.Client.RemoteEndPoint.ToString();
+        Console.WriteLine("Client has connected from {0}", address);
+
+        client.Close();
+        Console.WriteLine("Disconnect client {0}", address);
+      }
+      catch (SocketException e)
+      {
+        Console.WriteLine("SocketException: {0}", e);
+      }
+      finally
+      {
+        // Stop listening for new clients.
+        listener.Stop();
+        Console.WriteLine("Server shutdown");
+      }
     }
   }
 }
