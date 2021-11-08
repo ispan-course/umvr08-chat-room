9948f792f45bca9761ea20ec07c11d4fa931b44c
Arthur Pai <baidragoon@gmail.com>
2021-11-08 21:22:54 +0800

    refactor: extract client code to ChatClient class

--- ChatClient/Program.cs
+++ ChatClient/Program.cs
@@ -7,54 +7,32 @@ namespace ChatClient
   {
     public static void Main(string[] args)
     {
-      const string hostIP = "127.0.0.1";
-      const int port = 4099;
-
       Console.WriteLine("====================================");
-      var client = new TcpClient();
+      var client = new ChatCore.ChatClient();
 
-      try
+      var succeed = client.Connect("127.0.0.1", 4099);
+
+      if (!succeed)
       {
-        Console.WriteLine("Connecting to chat server {0}:{1}", hostIP, port);
-        client.Connect(hostIP, port);
+        return;
+      }
 
-        if (!client.Connected)
+      Console.WriteLine("<You can press any key to start entering text...>");
+
+      while (true)
+      {
+        var msg = Console.ReadLine();
+
+        if (msg == "exit")
         {
-          Console.WriteLine("Can't Connect to chat server");
-          return;
+          Console.WriteLine("<Bye...>");
+          client.Disconnect();
+          break;
         }
 
-        Console.WriteLine("Connected to chat server");
-
-        Console.WriteLine("Input anything then press Enter...");
-
-        while (true)
-        {
-          var msg = Console.ReadLine();
-          Send(client, msg);
-          Console.WriteLine("message sent: " + msg);
-        }
+        client.SendData(msg);
+        Console.WriteLine("<Message sent, press any key to start entering text again...>");
       }
-      catch (ArgumentNullException e)
-      {
-        Console.WriteLine("ArgumentNullException: {0}", e);
-      }
-      catch (SocketException e)
-      {
-        Console.WriteLine("SocketException: {0}", e);
-      }
-      finally
-      {
-        client.Close();
-        Console.WriteLine("Disconnected");
-      }
-    }
-
-    private static void Send(TcpClient client, string msg)
-    {
-      var requestBuffer = System.Text.Encoding.ASCII.GetBytes(msg);
-
-      client.GetStream().Write(requestBuffer, 0, requestBuffer.Length);
     }
   }
 }
---
+++ ChatCore/ChatClient.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Net.Sockets;
+
+namespace ChatCore
+{
+  public class ChatClient
+  {
+    private TcpClient m_client;
+
+    public ChatClient()
+    {
+    }
+
+    public bool Connect(string address, int port)
+    {
+      m_client = new TcpClient();
+
+      try
+      {
+        Console.WriteLine("Connecting to chat server {0}:{1}", address, port);
+        m_client.Connect(address, port);
+
+        Console.WriteLine("Connected to chat server");
+        return m_client.Connected;
+      }
+      catch (ArgumentNullException e)
+      {
+        Console.WriteLine("ArgumentNullException: {0}", e);
+        return false;
+      }
+      catch (SocketException e)
+      {
+        Console.WriteLine("SocketException: {0}", e);
+        return false;
+      }
+    }
+
+    public void Disconnect()
+    {
+      m_client.Close();
+      Console.WriteLine("Disconnected");
+    }
+
+    public void SendData(string message)
+    {
+      var requestBuffer = System.Text.Encoding.ASCII.GetBytes(message);
+
+      m_client.GetStream().Write(requestBuffer, 0, requestBuffer.Length);
+    }
+  }
+}
